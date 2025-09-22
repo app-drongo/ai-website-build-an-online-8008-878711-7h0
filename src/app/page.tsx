3486cf2b164fcbb36bb789ce81a531f54cd97c44
Drongo sas <148218896+drongo-sas@users.noreply.github.com>
2025-09-22 02:27:52 +0200

    Add 1 new component(s): pricing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@ import Hero from '@/components/sections/home/Hero'
 import Features from '@/components/sections/home/Features'
 import Productgrid from '@/components/sections/home/Productgrid'
 import Cta from '@/components/sections/home/Cta'
+import Pricing from '@/components/sections/newcomponents/Pricing'
 
 export default function HomePage() {
   return (
     <>
-      <Hero />
-      <Features />
-      <Productgrid />
-      <Cta />
+      <section id="pricing">
+        <Pricing />
+      </section>
     </>
   )
 }
